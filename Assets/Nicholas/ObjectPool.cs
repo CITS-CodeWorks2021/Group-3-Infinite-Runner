e095bb674f07254e706e916e7ad400624b76c226
NicholasWessinger <nicholaswessingerwork@gmail.com>
2021-07-13 10:08:22 -0400

    all the empty prefabs

---
+++ Assets/Nicholas/ObjectPool.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+
+    public GameObject[] prefabsToPool;
+    public int numToMake;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
